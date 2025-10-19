7e3e2c7504e87ca32f49c98709e17062406b5da6
Majed Alqudah <Majedjameel062@gmail.com>
2025-10-19 15:59:58 +0300

    adding endpoint to the server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,23 @@ import express, {Request , Response} from 'express';
 
 const app = express();
 
-app.get('/', (req : Request, res : Response) => {
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Express Server</title>
+    </head>
+    <body>
+      <h1>Welcome to the Express Server!</h1>
+      <a href="/ping">Ping</a>
+    </body>
+    </html>
+  `);
+});
+
+app.get('/ping', (req : Request, res : Response) => {
   res.status(200).send('pong');
 });
 
